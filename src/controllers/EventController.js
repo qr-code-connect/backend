14898817362e83ef4472090135bce8ab8942459a
teusmamf <martinsmateus382@gmail.com>
2025-05-31 12:07:54 -0300

    filtro eventos

--- src/controllers/EventController.js
+++ src/controllers/EventController.js
@@ -20,13 +20,20 @@ const createEvent = async (req,res) => {
 
 const getAllEvents = async (req, res) => {
     try {
-        const events = await getAllEventsService();
+        const filters = {
+            companyId: req.query.companyId,
+            eventId: req.query.eventId
+        };
+        
+        const events = await getAllEventsService(filters);
         res.status(200).json(events);
     } catch (error) {
-        res.status(500).json({ message: 'Error fetching customers', error: error.message });
-    };
+        res.status(500).json({ 
+            message: 'Error fetching events', 
+            error: error.message 
+        });
+    }
 };
-
 
 export {
     createEvent,
